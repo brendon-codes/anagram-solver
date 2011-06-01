f7ffaba3651a2335cb6d09a6d501823ab750a62a
Brendon Crawford <brendon@aphexcreations.net>
2011-06-01 14:31:14 -0700

    * Adding wordlist * Adding anagrap.py

---
+++ anagram.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""
+LAST.vc Anagram Solver
+
+Author: Brendon Crawford <brendon@last.vc>
+"""
+
+import sys
+
+
+def main():
+    """
+    Main Routine from Command Line
+
+    Returns: Int
+    """
+    data = sys.stdin.read()
+    print(data)
+    return 0
+
+
+def cycle(data):
+    """
+    Cycle
+    """
+    
+    return True
+
+
+if __name__ == '__main__':
+    sys.exit(main())
