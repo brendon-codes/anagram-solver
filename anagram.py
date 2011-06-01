a8e8d865108daff241c03cd5e04ed8630e1f87af
Brendon Crawford <brendon@aphexcreations.net>
2011-06-01 15:04:21 -0700

    Minor fixes

--- anagram.py
+++ anagram.py
@@ -6,6 +6,7 @@ LAST.vc Anagram Solver
 Author: Brendon Crawford <brendon@last.vc>
 """
 
+import os
 import sys
 
 
@@ -16,16 +17,50 @@ def main():
     Returns: Int
     """
     data = sys.stdin.read()
-    print(data)
+    wordlist = get_wordlist()
+    cycle(data, wordlist)
     return 0
 
 
-def cycle(data):
+def get_wordlist():
+    """
+    Gets wordlist
+    """
+    dname = os.path.dirname(__file__)
+    fname = os.path.realpath(dname + '/data/wordlist.txt')
+    fh = open(fname, 'r')
+    wordlist = [x.rstrip() for x in fh]
+    fh.close()
+    return wordlist
+
+
+def cycle(data, wordlist):
     """
     Cycle
     """
-    
+    bucket = ''
+    word = data
+    while len(data) > 0:
+        
+        word, extra = extract_word(data)
+        bucket += extra
     return True
+
+
+def find_match(word):
+    
+
+
+def extract_word(data):
+    """
+    Extract word
+    """
+    out = ''
+    idx = random.randint(0, len(data))
+    for i in xrange(len(data)):
+        if i != idx:
+            out += data[i]
+    return (out, data[idx])
 
 
 if __name__ == '__main__':
